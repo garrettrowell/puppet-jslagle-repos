7c289cf4c90c317af7431e34d694777ab71ae408
Jason Slagle <jslagle@gmail.com>
2012-11-16 15:25:18 -0500

    Adding spec for apt repo

--- spec/classes/repos_spec.rb
+++ spec/classes/repos_spec.rb
@@ -7,17 +7,31 @@ describe 'repos' do
     :enablepgsql => "true"
   } }
 
-  let(:facts) { {:osfamily => 'RedHat', :operatingsystemrelease => '6.2',
-    :hardwareisa => 'x86_64'
-  } }
 
-  describe 'Include pgsql' do
-    it { should include_class("repos::yum::pgsql") }
+  describe 'Redhat tests' do
+    let(:facts) { {:osfamily => 'RedHat', :operatingsystemrelease => '6.2',
+      :hardwareisa => 'x86_64'
+    } }
+
+    describe 'Include pgsql' do
+      it { should include_class("repos::yum::pgsql") }
+    end
+
+    describe 'Include puppetlabs' do
+      it { should include_class("repos::yum::puppetlabs") }
+    end
   end
 
-  describe 'Include puppetlabs' do
-    it { should include_class("repos::yum::puppetlabs") }
+  describe 'Ubuntu Tests' do
+    let (:facts) {{:osfamily => 'Debian', :lsbdistcodename => 'precise',
+      :hardwareisa => 'x86_64'
+    }}
+
+    describe 'Include puppetlabs' do
+      it { should include_class("repos::apt::puppetlabs") }
+    end
   end
+
 
 end
 
