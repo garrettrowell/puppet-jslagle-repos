4be7618027bffa359784cb401f00597153f52cd3
Garrett Rowell <Garrett2@cnwr.com>
2014-10-27 17:46:57 -0400

    added asterisk repo

---
+++ manifests/yum/asterisk.pp
@@ -0,0 +1,40 @@
+class repos::yum::asterisk(
+
+  $version       = '12',
+  $baseurl       = 'http://packages.asterisk.org/',
+
+) {
+
+  case $::osfamily {
+
+    RedHat: {
+
+      $majver    = $::operatingsystemmajrelease
+      $repoDef   = "asterisk-${version}"
+
+      # Asterisk Source Repo's
+      yumrepo { 'asterisk':
+        name     => $repoDef,
+        baseurl  => "${baseurl}/centos/${majver}/${repoDef}/${::hardwareisa}/",
+        enabled  => true,
+        gpgcheck => false,
+        descr    => "CentOS-${majver} - Asterisk - ${version}",
+      }
+
+      # Dependencies
+      yumrepo { 'asterisk-current':
+        name     => 'asterisk-current',
+        baseurl  => "${baseurl}/centos/${majver}/current/${::hardwareisa}/",
+        enabled  => true,
+        gpgcheck => false,
+        descr    => "CentOS-${majver} - Asterisk - Current",
+      }
+
+    }
+
+    default: { err("OSFamily ${::osfamily} not supported")}
+
+  }
+
+}
+
